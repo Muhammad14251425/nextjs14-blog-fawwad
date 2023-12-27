9760bad361d855667948f1a3eef96d452b18e1e2
Muhammad14251425 <muhammadfawwad88@gmail.com>
2023-12-27 15:30:37 +0500

    finished project

---
+++ app/lib/sanity.ts
@@ -0,0 +1,13 @@
+import {createClient } from "next-sanity"
+import imageUrlBuilder from '@sanity/image-url'
+export const client = createClient({
+    apiVersion : '2023-05-03',
+    dataset: 'production',
+    projectId: 'p4eislou',
+    useCdn: false,
+})
+
+const builder = imageUrlBuilder(client)
+export function urlFor(source:any) {
+    return builder.image(source);
+}
